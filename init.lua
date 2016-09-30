03e9f515333e51e0d85be9c9848fe272a24f6639
Thierry Chantier <thierry.chantier@semarchy.com>
2016-09-30 09:34:33 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+local http_api = minetest.request_http_api()
+if not http_api then
+   print("ERROR: in minetest.conf, this mod must be in secure.http_mods!")
+end
+
+minetest.register_node("connected:iotswitch", {
+	tiles = {"textures/tutorial_decowood.png"},
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+  on_rightclick = function (pos, node)
+		print("punch")
+    minetest.after(5, function()
+      http_api.fetch({
+        url = "http://localhost:3000/resources/feedsonoff",
+        timeout = 1
+      },
+      function (res)
+        print(dump(res))
+      end)
+    end)
+	end
+})
